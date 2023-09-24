8bb78206e2fa74200fe689287fbcd83c1f47877e
Tanmay <tanmaypednekar21@gmail.com>
2023-09-24 22:09:14 +0530

    added 'basename' attribute

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import {
 
 function App() {
   return (
-      <BrowserRouter>
+      <BrowserRouter basename={window.location.pathname || ''}>
         <Routes>
 
           <Route path={'/'} element={<Home/>}/>
